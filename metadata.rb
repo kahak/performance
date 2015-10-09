43a749ea1be92c70a8eed56d4ec00640e1a4d7b4
k.hakimzadeh@gmail.com <k.hakimzadeh@gmail.com>
2015-10-09 11:02:19 +0200

    Code generated by Karamel.

---
+++ metadata.rb
@@ -0,0 +1,30 @@
+name             'performance'
+maintainer       "performance"
+maintainer_email "k.hakimzadeh@gmail.com"
+license          "Apache v2.0"
+description      'Installs/Configures/Runs performance'
+version          "0.1"
+
+recipe            "performance::install", "Experiment setup for performance"
+recipe            "performance::experiment",  "configFile=; Experiment name: experiment"
+
+
+depends "kagent"
+
+
+
+%w{ ubuntu debian rhel centos }.each do |os|
+  supports os
+end
+
+
+
+attribute "performance/group",
+:description => "group parameter value",
+:type => "string"
+
+attribute "performance/user",
+:description => "user parameter value",
+:type => "string"
+
+
---
+++ recipes/experiment.rb
@@ -0,0 +1,17 @@
+#private_ip = my_private_ip()
+#public_ip = my_public_ip()
+
+script 'run_experiment' do
+  cwd "/home/performance"
+   user node['performance']['user']
+  group node['performance']['group']
+  interpreter "bash"
+  code <<-EOM
+/srv/hadoop/bin/hdfs dfs -rm -r -f /counts
+
+cd /usr/local/flink
+
+./bin/flink run -p 2 -j ./examples/flink-java-examples-0.9.1-WordCount.jar hdfs:///words/ hdfs://#{node.hadoop.nn.public_ips[0]}:29211/counts
+  EOM
+end
+
